77ebd3d72e624d2522993aeeef7c2ae94a3d7f27
saad ahla <93229038+Saad-AHLA@users.noreply.github.com>
2023-05-20 23:20:09 +0100

    FPL-14 Update Block.cpp

--- FilelessPELoader/Block.cpp
+++ FilelessPELoader/Block.cpp
@@ -2,7 +2,21 @@
 
 
 
+BOOL BlockNonMSDlls() {
+    // Define the policy
+    PROCESS_MITIGATION_BINARY_SIGNATURE_POLICY signaturePolicy = {};
+    signaturePolicy.MicrosoftSignedOnly = 1;
+    signaturePolicy.MitigationOptIn = 1;
 
+    // Set the process mitigation policy for loading only Microsoft DLLs
+    BOOL result = SetProcessMitigationPolicy(ProcessSignaturePolicy, &signaturePolicy, sizeof(signaturePolicy));
+    if (!result)
+    {
+        printf("Failed to set policy (%u)\n", GetLastError());
+        return FALSE;
+    }
+    return TRUE;
+}
 
 
 void SetProcessSecurityDescriptor() {
